26e7270d527f8f6a59b56f2604fcfabba6dff525
Excecutif <68136140+ExecuteIF@users.noreply.github.com>
2023-02-25 20:21:46 +0800

    Create v2c.hpp

---
+++ v2c.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+#include <fstream>
+#include <thread>
+#include <windows.h>
+
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+#define KEY_DOWN(VK_NONAME) ((GetAsyncKeyState(VK_NONAME) & 0x8000) ? 1:0)
